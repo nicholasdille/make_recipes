9d5c7525abce645aa4c9658cf9017fb3c7455138
Nicholas Dille <nicholas.dille@mailbox.org>
2019-07-02 14:39:14 +0200

    House keeping

--- Makefile
+++ Makefile
@@ -1,23 +1,30 @@
+########## Global
+OWNER=nicholasdille
+PROJECT=insulatr
 PWD=$(shell pwd)
 TOOLS=$(PWD)/tools
 SEMVER=$(shell test -f $(TOOLS)/semver && test -x $(TOOLS)/semver || ( mkdir -p $(TOOLS) && curl -sLf https://github.com/fsaintjacques/semver-tool/raw/2.1.0/src/semver > $(TOOLS)/semver && chmod +x $(TOOLS)/semver && echo $(TOOLS)/semver))
 
-GITHUB_OWNER=nicholasdille
-GITHUB_REPOSITORY=insulatr
+########## Docker
+DOCKER_OWNER=$(OWNER)
+DOCKER_REPOSITORY=$(PROJECT)
+DOCKER_TAG=1.0
+include Makefile.docker
+
+########## Release
+GITHUB_OWNER=$(OWNER)
+GITHUB_REPOSITORY=$(PROJECT)
 RELEASE_VERSION=1.0.4
 MAJOR_VERSION = $(shell $(SEMVER) get major $(RELEASE_VERSION))
 MINOR_VERSION = $(shell $(SEMVER) get minor $(RELEASE_VERSION))
-
 RELEASE_ASSETS=$(wildcard Makefile.*)
-
-DOCKER_OWNER=nicholasdille
-DOCKER_REPOSITORY=test
-DOCKER_TAG=1.0
-
-include Makefile.docker
 include Makefile.release
-#include Makefile.go
 
+########## Go
+GO_PACKAGE=$(PROJECT)
+include Makefile.go
+
+########## Custom
 .PHONY: docker
 
 .SECONDARY:
--- Makefile.go
+++ Makefile.go
@@ -1,15 +1,13 @@
-OWNER    = nicholasdille
-PACKAGE  = insulatr
-IMAGE    = $(OWNER)/$(PACKAGE)
-STATIC   = insulatr-$(shell uname -m)
+########## Required variables
+# GO_PACKAGE - container the name of the binary
+
+########## Variables
+STATIC   = $(GO_PACKAGE)-$(shell uname -m)
 SOURCE   = $(shell echo *.go)
 PWD      = $(shell pwd)
 BIN      = $(PWD)/bin
-TOOLS    = $(PWD)/tools
 GOMOD    = $(PWD)/go.mod
 GOFMT    = gofmt
-SEMVER   = $(TOOLS)/semver
-BUILDDEF = insulatr.yaml
 
 GIT_COMMIT = $(shell git rev-list -1 HEAD)
 BUILD_TIME = $(shell date +%Y%m%d-%H%M%S)
@@ -17,27 +15,21 @@ GIT_TAG = $(shell git describe --tags 2>/dev/null)
 
 M = $(shell printf "\033[34;1m▶\033[0m")
 
-.DEFAULT_GOAL := $(PACKAGE)
+########## Targets: Global
 
-.PHONY: clean prepare deps deppatch depupdate deptidy format linter check static binary check-docker docker test run check-changes $(PACKAGE) bump-% build-% release-% tag-% changelog changelog-% release $(IMAGE)-% check-tag push-% latest-%
+.PHONY: clean prepare deps deppatch depupdate deptidy format linter check
 
 .SECONDARY:
 
 clean: clean-docker; $(info $(M) Cleaning...)
 	@rm -rf $(BIN)
-	@rm -rf $(TOOLS)
 
-prepare: | $(BIN) $(TOOLS) $(SEMVER)
+prepare: | $(BIN)
 
 $(BIN): ; $(info $(M) Preparing binary...)
 	@mkdir -p $(BIN)
 
-$(TOOLS): ; $(info $(M) Preparing tools...)
-	@mkdir -p $(TOOLS)
-
-##################################################
-# TOOLS
-##################################################
+########## Targets: Tools
 
 deps: $(GOMOD)
 
@@ -64,17 +56,7 @@ lint: ; $(info $(M) Running linter...)
 deptree: ; $(info $(M) Creating dependency tree...)
 	@depth .
 
-semver: $(SEMVER)
-
-$(SEMVER): $(TOOLS) ; $(info $(M) Installing semver...)
-	@test -f $@ && test -x $@ || ( \
-		curl -sLf https://github.com/fsaintjacques/semver-tool/raw/2.1.0/src/semver > $@; \
-		chmod +x $@; \
-	)
-
-##################################################
-# BUILD
-##################################################
+########## Targets: Build
 
 check: format lint
 
@@ -84,22 +66,8 @@ check: format lint
 %.asc: % ; $(info $(M) Creating signature for $*...)
 	@gpg --local-user $$(git config --get user.signingKey) --sign --armor --detach-sig --yes $*
 
-binary $(PACKAGE): $(BIN)/$(PACKAGE)
-
 $(BIN)/$(PACKAGE): $(SOURCE) | prepare ; $(info $(M) Building $(PACKAGE)...)
 	@go build -ldflags "-s -w -X main.GitCommit=$(GIT_COMMIT) -X main.BuildTime=$(BUILD_TIME) -X main.Version=$(GIT_TAG)" -o $@ $(SOURCE)
 
-static: $(BIN)/$(STATIC)
-
 $(BIN)/$(STATIC): $(SOURCE) | prepare ; $(info $(M) Building static $(PACKAGE)...)
 	@CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-s -w -X main.GitCommit=$(GIT_COMMIT) -X main.BuildTime=$(BUILD_TIME) -X main.Version=$(GIT_TAG)" -o $@ $(SOURCE)
-
-##################################################
-# TEST
-##################################################
-
-scp-%: $(BIN)/$(PACKAGE) ; $(info $(M) Copying to $*)
-	@tar -cz bin/$(PACKAGE) $(BUILDDEF) | ssh $* tar -xvz
-
-ssh-%: scp-% ; $(info $(M) Running remotely on $*)
-	@ssh $* ./bin/$(PACKAGE) --file $(BUILDDEF) $(PARAMS)
